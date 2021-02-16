32c049d0fdab9660df3b2065987931bd6b42b3e4
edwardhsueh <edward.hsueh@gmail.com>
2021-02-16 18:08:10 +0800

    0216

---
+++ Code/Chapter02/Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names; // can reference any array of strings
+            // allocating memory for four strings in an array
+            names = new string[4];
+            // storing items at index positions
+            names[0] = "Kate";
+            names[1] = "Jack";
+            names[2] = "Rebecca";
+            names[3] = "Tom";
+            // looping through the names
+            for (int i = 0; i < names.Length; i++)
+            {
+                // output the item at index position i
+                Console.WriteLine(names[i]);        
+            }
+            int[] intArray = new int[4]; // can reference any array of strings
+            // storing items at index positions
+            intArray[0] = 5;
+            intArray[1] = 7;
+            intArray[2] = 11;
+            intArray[3] = 9;
+            // looping through the names
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                // output the item at index position i
+                Console.WriteLine(intArray[i]);        
+            }            
+        }
+    }
+}
---
+++ Code/Chapter02/Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.88; // storing a double in an object
+            object name = "Amir"; // storing a string in an object
+            Console.WriteLine($"{name} is {height} metres tall.");
+            // int length1 = name.Length; // gives compile error!
+            int length2 = ((string)name).Length; // tell compiler it is a string
+            Console.WriteLine($"{name} has {length2} characters.");
+            // storing a string in a dynamic object
+            dynamic anotherName = "Ahmed";
+            int length3 = anotherName.Length; // tell compiler it is a string
+            Console.WriteLine($"{anotherName} has {length3} characters.");
+        }
+    }
+}
