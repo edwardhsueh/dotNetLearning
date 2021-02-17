b015b27a05d099815c0003b863ea82b9725a13c0
edwardhsueh <edward.hsueh@gmail.com>
2021-02-17 22:04:18 +0800

    0217

---
+++ Code/Chapter03/Operators/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using static System.Console;
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int e = 11;
+            int f = 3;
+            WriteLine($"e is {e}, f is {f}");
+            WriteLine($"e + f = {e + f}");
+            WriteLine($"e - f = {e - f}");
+            WriteLine($"e * f = {e * f}");
+            WriteLine($"e / f = {e / f}");
+            WriteLine($"e % f = {e % f}");            
+            double g = 11.0;
+            WriteLine($"g is {g:N}, f is {f}");
+            WriteLine($"g / f = {g / f}");
+            WriteLine($"f / g = {f / g}");
+            bool a = true;
+            bool b = false;
+            WriteLine($"   a  |   b ");
+            WriteLine($"{a,5} | {b,-5}");
+            WriteLine($"AND | a | b ");
+            WriteLine($"a | {a & a,-5} | {a & b,-5} ");
+            WriteLine($"b | {b & a,-5} | {b & b,-5} ");
+            WriteLine();
+            WriteLine($"OR | a | b ");
+            WriteLine($"a | {a | a,-5} | {a | b,-5} ");
+            WriteLine($"b | {b | a,-5} | {b | b,-5} ");
+            WriteLine();
+            WriteLine($"XOR | a | b ");
+            WriteLine($"a | {a ^ a,-5} | {a ^ b,-5} ");
+            WriteLine($"b | {b ^ a,-5} | {b ^ b,-5} ");
+            int age = 47;
+            // How many operators in the following statement?
+            char firstDigit = age.ToString()[0];
+            WriteLine(firstDigit);
+            WriteLine(nameof(age));
+            WriteLine(sizeof(int));
+            // add and remove the "" to change the behavior
+            object o = 3;
+            int j = 4;
+            if (o is int i)
+            {
+                WriteLine($"{i} x {j} = {i * j}");
+            }
+            else
+            {
+                WriteLine("o is not an int so it cannot multiply!");
+            }   
+                
+        }
+    }
+}
---
+++ Code/Chapter03/Switch/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string path = "/Users/markjprice/Code/Chapter03";
+            string path = @"F:\github\dotNetLearning\Code\Chapter03";
+            Write("Press R for readonly or W for write: ");
+            ConsoleKeyInfo key = ReadKey();
+            WriteLine();
+            Stream s = null;
+            if (key.Key == ConsoleKey.R)
+            {
+                s = File.Open(
+                Path.Combine(path, "file.txt"),
+                FileMode.OpenOrCreate,
+                FileAccess.Read);
+            }
+            else
+            {
+                s = File.Open(
+                Path.Combine(path, "file.txt"),
+                FileMode.OpenOrCreate,
+                FileAccess.Write);
+            }
+            string message = string.Empty;
+            WriteLine($"s={s}");
+            switch (s)
+            {
+                case FileStream fs when fs.CanWrite:
+                    message = $"The stream is a file that I can write to.{fs.CanWrite}";
+                    break;
+                case FileStream when (s.CanWrite == false):
+                    message = $"The stream is a read-only file.{s.CanWrite}";
+                    break;
+                case MemoryStream ms:
+                    message = "The stream is a memory address.";
+                    break;
+                default: // always evaluated last despite its current position
+                    message = "The stream is some other type.";
+                    break;
+                case null:
+                    message = "The stream is null.";
+                    break;
+            }
+            WriteLine(message);
+
+            string[] names = { "Adam", "Barry", "Charlie" };
+            foreach (string name in names)
+            {
+                WriteLine($"{name} has {name.Length} characters.");
+            }
+            for (int y = 1; y <= 10; y++)
+            {
+                WriteLine(y);
+            }
+        }
+    }
+}
