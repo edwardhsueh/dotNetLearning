6b70af36b184df46b9552711f0ce3a03fc15f3ed
edwardhsueh <edward.hsueh@gmail.com>
2021-03-02 17:40:36 +0800

    0302 final

--- Code/Chapter06/Exercise02Lib/Circle.cs
+++ Code/Chapter06/Exercise02Lib/Circle.cs
@@ -1,10 +1,10 @@
 using System;
-
+using static System.Console;
 namespace Exercise02Lib
 {
     public class Circle : Shape
     {
-      public Circle(double width) : base(width: width, height:width){}
+      public Circle(double width) : base(width: width, height:width){ WriteLine("base constructor used"); }
       public override double Area
       {
         get {
