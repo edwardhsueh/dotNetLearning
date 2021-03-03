493ce4d70a2a7cc2c5a1c9b3a634c140315614cf
edwardhsueh <edward.hsueh@gmail.com>
2021-03-03 18:17:32 +0800

    0303 NB

---
+++ Code/Chapter07/DotNetCoreEverywhere/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DotNetCoreEverywhere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I Can run everyWhere!");
+            Debug.WriteLine("Debug says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+        }
+    }
+}
