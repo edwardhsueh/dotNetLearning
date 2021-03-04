697b1eeb117c289f3bc740c3e550e190f02c89f4
edwardhsueh <edward.hsueh@gmail.com>
2021-03-04 14:46:44 +0800

    0304 AF

---
+++ Code/Chapter08/WorkingWithDictionaries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+namespace WorkingWithDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keywords = new Dictionary<string, string>(){
+                { "byte", "unsinged 8-bit Int"},
+                { "dobule", "Double precision floating point number"},
+            };
+            keywords.Add("int", "32-bit integer data type");
+            keywords.Add("long", "64-bit integer data type");
+            keywords.Add("float", "Single precision floating point number");
+            WriteLine("Keywords and their definitions");
+            foreach (KeyValuePair<string, string> item in keywords)
+            {
+                WriteLine($" {item.Key}: {item.Value}");
+            }
+            WriteLine($"The definition of long is {keywords["long"]}");
+
+            keywords.Remove("byte", out string removed);
+            WriteLine($"key: bytes, value:{removed} is removed");
+            WriteLine("after Remove: Keywords and their definitions");
+            foreach (KeyValuePair<string, string> item in keywords)
+            {
+                WriteLine($" {item.Key}: {item.Value}");
+            }
+        }
+    }
+}
--- Code/Chapter08/WorkingWithRegularExpressions/Program.cs
+++ Code/Chapter08/WorkingWithRegularExpressions/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-
+using System.Text.RegularExpressions;
+using static System.Console;
 namespace WorkingWithRegularExpressions
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // regular expression should use verbatim string
+            // Checking for digits entered as text
+            Write("Enter your age: ");
+            string input = ReadLine();
+            var ageChecker = new Regex(@"^\d{1,4}$");
+            if (ageChecker.IsMatch(input))
+            {
+                WriteLine("Thank you!");
+            }
+            else
+            {
+                WriteLine($"This is not a valid age: {input}");
+            }
+            WriteLine("Test regular expression for splitting:");
+            string films = "\"M1onsters, Inc.\",\"I, Tonya\",\"Lock, Stock and Two Smoking Barrels\"";
+            string[] filmsDumb = films.Split(',');
+            WriteLine("Dumb attempt at splitting:");
+            foreach (string film in filmsDumb)
+            {
+                WriteLine(film);
+            }
+            var csv = new Regex(
+            "(?:^|,)(?=[^\"]|(\")?)\"?((?(1)[^\"]*|[^,\"]*))\"?(?=,|$)");
+            MatchCollection filmsSmart = csv.Matches(films);
+            WriteLine("Smart attempt at splitting:");
+            foreach (Match film in filmsSmart)
+            {
+                WriteLine(film.Groups[2].Value);
+            }
         }
     }
 }
