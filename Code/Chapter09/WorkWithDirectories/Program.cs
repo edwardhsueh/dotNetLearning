c9982c082d3f2bb149a37462f094178d84b61390
edwardhsueh <edward.hsueh@gmail.com>
2021-03-10 17:43:39 +0800

    0310 AF

---
+++ Code/Chapter09/WorkWithDirectories/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO; // types for managing the filesystem
+using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+
+namespace WorkWithDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkWithDirectories();
+        }
+    }
+}
