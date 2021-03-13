7d1b8bef3d0087d23d8434e18188bc132a4750b2
edwardhsueh <edward.hsueh@gmail.com>
2021-03-13 17:59:24 +0800

    0313 N OK

--- Code/Chapter09/WorkingWithStreams/Program.cs
+++ Code/Chapter09/WorkingWithStreams/Program.cs
@@ -14,62 +14,64 @@ namespace WorkingWithStreams
         static string[] callsigns = new string[] {
         "Husker", "Starbuck", "Apollo", "Boomer",
         "Bulldog", "Athena", "Helo", "Racetrack" };
-/// <summary>
-/// Compress XML Document
-/// </summary>
-static void WorkWithCompression()
-{
-// compress the XML output
+        /// <summary>
+        /// Compress XML Document
+        /// </summary>
+        static void WorkWithCompression()
+        {
+        // compress the XML output
 
-    string gzipFilePath = Combine(
-    CurrentDirectory, "streams.gzip");
-    FileStream gzipFile = File.Create(gzipFilePath);
-    using (GZipStream compressor = new GZipStream(
-    gzipFile, CompressionMode.Compress))
-    {
-        using (XmlWriter xmlGzip = XmlWriter.Create(compressor))
-        {
-            xmlGzip.WriteStartDocument();
-            xmlGzip.WriteStartElement("callsigns");
-            foreach (string item in callsigns)
+            string gzipFilePath = Combine(
+            CurrentDirectory, "streams.gzip");
+            FileStream gzipFile = File.Create(gzipFilePath);
+            using (GZipStream compressor = new GZipStream(
+            gzipFile, CompressionMode.Compress))
             {
-                xmlGzip.WriteElementString("callsign", item);
-            }
-            // the normal call to WriteEndElement is not necessary
-            // because when the XmlWriter disposes, it will
-            // automatically end any elements of any depth
-            xmlGzip.WriteEndElement();
-        }
-    } // also closes the underlying stream
-            // output all the contents of the compressed file
-    WriteLine("{0} contains {1:N0} bytes.",
-    gzipFilePath, new FileInfo(gzipFilePath).Length);
-    WriteLine($"The compressed contents:");
-    WriteLine(File.ReadAllText(gzipFilePath));
-    // read a compressed file
-    WriteLine("Reading the compressed XML file:");
-    gzipFile = File.Open(gzipFilePath, FileMode.Open);
-    using (GZipStream decompressor = new GZipStream(
-    gzipFile, CompressionMode.Decompress))
-    {
-        using (XmlReader reader = XmlReader.Create(decompressor))
-        {
-            while (reader.Read()) // read the next XML node
-            {
-                // check if we are on an element node named callsign
-                if ((reader.NodeType == XmlNodeType.Element)
-                && (reader.Name == "callsign"))
+                using (XmlWriter xmlGzip = XmlWriter.Create(compressor))
                 {
-                    reader.Read(); // move to the text inside element
-                    WriteLine($"{reader.Value}"); // read its value
+                    xmlGzip.WriteStartDocument();
+                    xmlGzip.WriteStartElement("callsigns");
+                    foreach (string item in callsigns)
+                    {
+                        xmlGzip.WriteElementString("callsign", item);
+                    }
+                    // the normal call to WriteEndElement is not necessary
+                    // because when the XmlWriter disposes, it will
+                    // automatically end any elements of any depth
+                    xmlGzip.WriteEndElement();
+                }
+            } // also closes the underlying stream
+                    // output all the contents of the compressed file
+            WriteLine("{0} contains {1:N0} bytes.",
+            gzipFilePath, new FileInfo(gzipFilePath).Length);
+            WriteLine($"The compressed contents:");
+            WriteLine(File.ReadAllText(gzipFilePath));
+            // read a compressed file
+            WriteLine("Reading the compressed XML file:");
+            gzipFile = File.Open(gzipFilePath, FileMode.Open);
+            using (GZipStream decompressor = new GZipStream(
+            gzipFile, CompressionMode.Decompress))
+            {
+                using (XmlReader reader = XmlReader.Create(decompressor))
+                {
+                    while (reader.Read()) // read the next XML node
+                    {
+                        // check if we are on an element node named callsign
+                        if ((reader.NodeType == XmlNodeType.Element)
+                        && (reader.Name == "callsign"))
+                        {
+                            reader.Read(); // move to the text inside element
+                            WriteLine($"{reader.Value}"); // read its value
 
+                        }
+                    }
                 }
             }
         }
-    }
-}
 
-
+        /// <summary>
+        /// XML write to plain file
+        /// </summary>
         static void WorkWithXml()
         {
             // define a file to write to
@@ -107,6 +109,9 @@ static void WorkWithCompression()
                 }
             }
         }
+        /// <summary>
+        /// Write to text file with UTF-8
+        /// </summary>
         static void WorkWithText()
         {
             // define a file to write to
