771197849ff19e3b58e951497ecc4550cd37f55c
edwardhsueh <edward.hsueh@gmail.com>
2021-03-21 17:16:32 +0800

    0321 GitIgnore

---
+++ Code/Chapter11/testGitIgnore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testGitIgnore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
