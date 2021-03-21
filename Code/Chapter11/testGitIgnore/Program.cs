4c6f146fc17205f9076f238e7d1aaca8e7af7296
edwardhsueh <edward.hsueh@gmail.com>
2021-03-21 17:18:56 +0800

    0321 TestGitIgnore

--- Code/Chapter11/testGitIgnore/Program.cs
+++ Code/Chapter11/testGitIgnore/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
-namespace testGitIgnore
+using Edward.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+namespace Exercise
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // using is important
+
+            using (var dbContext = new EdwardDb()){
+                dbContext.Database.EnsureDeleted();
+                var sql = dbContext.Database.GenerateCreateScript();
+                string fileName = Path.Combine(Environment.CurrentDirectory, "output.sql");
+                Console.Write("Write Create Database SQL to file:{0}",fileName);
+                using (var fsWrite = File.CreateText(fileName)){
+                    fsWrite.WriteLine(sql);
+                }
+                Console.Write("Create Database SQL:"+sql);
+                dbContext.Database.EnsureCreated();
+            }
         }
+
     }
 }
