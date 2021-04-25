fdd6699f173c6a93d35040a293b7045342e50276
edwardhsueh <edward.hsueh@gmail.com>
2021-04-25 16:13:29 +0800

    0425 Razor

--- Code/PracticalApps/NorthwindWeb/Startup.cs
+++ Code/PracticalApps/NorthwindWeb/Startup.cs
@@ -16,6 +16,8 @@ namespace NorthwindWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // add statements to add Razor Pages and its related services like model binding, authorization, anti-forgery, views, and tag helpers
+            services.AddRazorPages();    
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,9 +38,13 @@ namespace NorthwindWeb
             app.UseRouting();
             // redirect HTTP requests to HTTPS
             app.UseHttpsRedirection();
+            // The call to UseDefaultFiles must be before the call to UseStaticFiles, or it won't work!
+            app.UseDefaultFiles(); // index.html, default.html, and so on
+            app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapRazorPages();
+                endpoints.MapGet("/hello", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
