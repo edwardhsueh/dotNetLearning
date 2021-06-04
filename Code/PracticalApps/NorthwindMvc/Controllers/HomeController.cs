08ba846ec601089f84d04d195da240d493e3f102
edwardhsueh <edward.hsueh@gmail.com>
2021-06-04 16:04:59 +0800

    0604

--- Code/PracticalApps/NorthwindMvc/Controllers/HomeController.cs
+++ Code/PracticalApps/NorthwindMvc/Controllers/HomeController.cs
@@ -27,12 +27,39 @@ namespace NorthwindMvc.Controllers
             Categories = db.Categories.ToList(), 
             Products = db.Products.ToList()
             };            
+            // pass model to View
             return View(model);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult ProductDetail(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return NotFound("You must pass a product ID in the route, for example, /Home/ProductDetail/21");
+            }
+            // var model = db.Products
+            //     .SingleOrDefault(p => p.ProductId == id);
+            var model = (from p in db.Products 
+                        where p.ProductId == id
+                        select p).ToList();
+            if (model == null)
+            {
+                return NotFound($"Product with ID of {id} not found.");
+            }
+            if(model.Count == 0){
+                return NotFound($"Product with ID of {id} not found.");
+            }
+            else if(model.Count > 1){
+                return NotFound($"Product with ID of {id} more than 1.");
+            }
+            else {
+            return View(model.First()); // pass model to view and then return result
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
