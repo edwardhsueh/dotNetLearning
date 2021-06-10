17b4527accb5b4b865daab471ae6f4c46f82af80
edwardhsueh <edward.hsueh@gmail.com>
2021-06-10 17:24:59 +0800

    0610 httpService

---
+++ Code/PracticalApps/NorthwindService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NorthwindService.Controllers
+{
+// The [Route] attribute registers the weatherforecast relative URL for clients to use to make HTTP requests that will be handled by this controller. For example, an HTTP request for http://localhost:5001/weatherforecast/ would be handled by this controller. Some developers like to prefix the controller name with api/, which is a convention to differentiate between MVC and Web API in mixed projects. If you use [controller] as shown, it uses the characters before Controller in the class name, in this case, WeatherForecast, or you can simply enter a different name without the square brackets, for example, [Route("api/forecast")].
+// The [ApiController] attribute was introduced with ASP.NET Core 2.1 and it enables REST-specific behavior for controllers, like automatic HTTP 400 responses for invalid models, as you will see later in this chapter.    
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        // public IEnumerable<WeatherForecast> Get()
+        // {
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
+        // GET /weatherforecast
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get() // original method
+        {
+            return Get(5); // five day forecast
+        }
+        // GET /weatherforecast/7
+        [HttpGet("{days:int}")]
+        public IEnumerable<WeatherForecast> Get(int days) // new method
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, days).Select(index =>
+                new WeatherForecast
+                {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+                })
+                .ToArray();
+        }
+    }
+}
---
+++ Code/PracticalApps/NorthwindService/Startup.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using static System.Console;
+using NorthwindService.Repositories;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+namespace NorthwindService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public static readonly ILoggerFactory loggerFactory = new LoggerFactory(new[] {
+            new ConsoleLoggerProvider()
+        });
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string databasePath = Path.Combine("..", "Northwind.db");
+            // add databaseLogger: tie-up DbContext with LoggerFactory object
+            services.AddDbContext<Northwind>(options => 
+            options.UseLoggerFactory(loggerFactory).UseSqlite($"Data Source={databasePath}"));            
+
+            // services.AddControllers();
+            services.AddControllers(options =>
+            {
+                WriteLine("Default output formatters:");
+                foreach(IOutputFormatter formatter in options.OutputFormatters)
+                {
+                var mediaFormatter = formatter as OutputFormatter;
+                if (mediaFormatter == null)
+                {
+                    WriteLine($" {formatter.GetType().Name}");
+                }
+                else // OutputFormatter class has SupportedMediaTypes
+                {
+                    WriteLine(" {0}, Media types: {1}",
+                    arg0: mediaFormatter.GetType().Name, 
+                    arg1: string.Join(", ", 
+                        mediaFormatter.SupportedMediaTypes));
+                }
+                }
+            })
+            .AddXmlDataContractSerializerFormatters()
+            .AddXmlSerializerFormatters()
+            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NorthwindService", Version = "v1" });
+            });
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NorthwindService v1"));
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json",
+                        "Northwind Service API Version 1");
+                    c.SupportedSubmitMethods(new[] { 
+                        SubmitMethod.Get, SubmitMethod.Post,
+                        SubmitMethod.Put, SubmitMethod.Delete });
+                });                
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
